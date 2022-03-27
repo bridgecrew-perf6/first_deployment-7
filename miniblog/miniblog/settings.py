394767183496ef57c7d267212d7d0e79557ddc72
sekharmohanty <sekharmohanty561999@gmail.com>
2022-03-27 07:03:48 +0530

    adding files with changes

--- miniblog/miniblog/settings.py
+++ miniblog/miniblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y69o$4kc_t6#pzzz#earatb&@hwvp&poitcn_$79y10f!l#*06'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,9 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# STATIC_ROOT= BASE_DIR / 'static'
